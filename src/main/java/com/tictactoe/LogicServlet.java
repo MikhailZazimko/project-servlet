d9c0145d617a20908af45ade43443b1d0da00c1f
MikhailZazimko <mzazimko@icloud.com>
2022-08-23 18:02:20 +0300

    Commit 2. Make 80% app

--- src/main/java/com/tictactoe/LogicServlet.java
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -4,13 +4,31 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "LogicServlet", value = "/logic")
 public class LogicServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        HttpSession currentSession = request.getSession();
+        Field field = extractField(currentSession);
         int index = getSelectedIndex(request);
+
+        Sign currentSign = field.getField().get(index);
+        if(Sign.EMPTY!=currentSign){
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
+            dispatcher.forward(request,response);
+            return;
+        }
+        field.getField().put(index,Sign.CROSS);
+        int emptyFieldIndex = field.getEmptyFieldIndex();
+        if(emptyFieldIndex>=0){
+            field.getField().put(emptyFieldIndex,Sign.NOUGHT);
+        }
+        List<Sign> data = field.getFieldData();
+        currentSession.setAttribute("field", field);
+        currentSession.setAttribute("data",data);
+
         response.sendRedirect("/index.jsp");
     }
     private Field extractField(HttpSession currentSession){
